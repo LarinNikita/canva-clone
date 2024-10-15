ba1ce9103d5f721df53b95a87f0907c316643f23
Larin Nikita <l_nikita2000@mail.ru>
2024-10-15 09:43:33 +0700

    editor shape sidebar

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -6,11 +6,12 @@ import { fabric } from 'fabric';
 
 import { useEditor } from '@/features/editor/hooks/use-editor';
 
+import { ActiveTool } from '@/features/editor/types';
 import { Navbar } from '@/features/editor/components/navbar';
+import { Footer } from '@/features/editor/components/footer';
 import { Sidebar } from '@/features/editor/components/sidebar';
 import { Toolbar } from '@/features/editor/components/toolbar';
-import { Footer } from '@/features/editor/components/footer';
-import { ActiveTool } from '@/features/editor/types';
+import { ShapeSidebar } from '@/features/editor/components/shape-sidebar';
 
 export const Editor = () => {
     const [activeTool, setActiveTool] = useState<ActiveTool>('select');
@@ -63,6 +64,10 @@ export const Editor = () => {
             />
             <div className="absolute top-[68px] flex h-[calc(100%-68px)] w-full">
                 <Sidebar
+                    activeTool={activeTool}
+                    onChangeActiveTool={onChangeActiveTool}
+                />
+                <ShapeSidebar
                     activeTool={activeTool}
                     onChangeActiveTool={onChangeActiveTool}
                 />
