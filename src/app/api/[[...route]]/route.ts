90f1d061cfd71ea5273ab4698f04128833d987b5
Larin Nikita <l_nikita2000@mail.ru>
2024-10-19 12:33:29 +0700

    setup hono and tanstack query

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,20 @@
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+
+//? Revert to "edge" if you planning on running on the edge
+export const runtime = 'nodejs';
+
+const app = new Hono().basePath('/api');
+
+app.get('/test', c => {
+    return c.json({ message: 'Hono test api!' });
+});
+
+app.get('/user/:name', c => {
+    const name = c.req.param('name');
+    return c.json({ userName: name });
+});
+
+export const GET = handle(app);
+
+export type AppType = typeof app;
