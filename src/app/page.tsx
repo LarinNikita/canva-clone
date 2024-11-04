b4017450e88d0af4395818138aec05d069736385
Larin Nikita <l_nikita2000@mail.ru>
2024-11-04 11:37:05 +0700

    auth.js setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-export default function Home() {
-  return (
-    <div></div>
-  );
+import { auth } from '@/auth';
+
+export default async function Home() {
+    const session = await auth();
+
+    return <div>{JSON.stringify(session)}</div>;
 }
